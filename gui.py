0aca72d2f524daca7b9fe2080a723642e7e4978e
andrei <at3006@yahoo.com>
2025-12-17 14:40:34 +0200

    Fixed TTS threading; Added more colours

--- gui.py
+++ gui.py
@@ -3,8 +3,8 @@ from tkinter import filedialog, messagebox
 from PIL import Image, ImageTk
 import pyttsx3
 import threading # for better performance (TTS not to block gui)
-
 from sklearn.cluster import KMeans
+import queue
 
 from processor import ImageProcessor
 from utils import rgb_to_hex, get_closest_color_name
@@ -14,17 +14,71 @@ class picasApp:
     def __init__(self, root):
         self.root = root
         self.root.title("PICAS")
-        self.root.geometry("600x700")
+        self.root.geometry("900x600")
         self.processor = ImageProcessor()
 
+        # --- FIX TTS 1: Configurare Coada si Motor ---
+        self.speech_queue = queue.Queue()
+        self.tts_engine = None
+
+        # starting unique thread for audio
+        # daemon=True -> closes when the app is closed
+        threading.Thread(target=self._tts_worker, daemon=True).start()
+
+        self._setup_ui()
+
+    """
+    Special function to run in a sperate thread permanently.
+    It solves the problem with the TTS, now the audio (speaking) works normal.
+    """
+    def _tts_worker(self):
         try:
-            self.tts_engine = pyttsx3.init()
-            self.tts_engine.setProperty('rate', 150)
+            engine = pyttsx3.init()
+            engine.setProperty('rate', 150)
+            self.tts_engine = engine
+
+            while True:
+                text = self.speech_queue.get()
+
+                if text is None:
+                    break
+
+                #speak the text
+                try:
+                    engine.say(text)
+                    engine.runAndWait()
+                except:
+                    pass
+
+                # mark task done
+                self.speech_queue.task_done()
+
         except Exception as e:
-            print("Could not initialize pyttsx3 module", e)
-            self.tts_engine = None
+            print("Error in the audio thread", e)
+
+    def _setup_ui(self):
+        header_frame = tk.Frame(self.root, pady=10)
+        header_frame.pack()
+
+        btn_load = tk.Button(header_frame, text="Uplaod Image", command=self.upload_image,
+                             bg="#4a90e2", fg="white", font=("Arial", 12, "bold"))
+        btn_load.pack()
+
+        self.image_container = tk.Label(self.root, text="No selected image", bg="#f0f0f0")
+        self.image_container.pack(pady=20, expand=True)
+
+        self.palette_frame = tk.Frame(self.root)
+        self.palette_frame.pack(pady=20, fill="x")
+
+        lbl_info = tk.Label(self.palette_frame, text="Dominant colours (click for audio):",
+                            font=("Arial", 10, "italic"))
+        lbl_info.pack(pady=5)
+
+        self.swatches_container = tk.Frame(self.palette_frame)
+        self.swatches_container.pack()
 
     """Uploading a picture"""
+
     def upload_image(self):
         file_path = filedialog.askopenfilename(
             filetypes=[("Image Files", "*.jpg;*.jpeg;*.png;*.bmp")]
@@ -43,12 +97,12 @@ class picasApp:
     def display_image(self):
         array_img = self.processor.get_display_image(max_size=(500, 400))
 
-        if array_img is None:
+        if array_img is not None:
             pil_img = Image.fromarray(array_img)
             img_tk = ImageTk.PhotoImage(pil_img)
 
             self.image_container.configure(image=img_tk, text="")
-            self.image_container.image = img_tk # garbage collector reference
+            self.image_container.image = img_tk  # garbage collector reference
 
     def generate_palette(self):
         for widget in self.swatches_container.winfo_children():
@@ -64,40 +118,27 @@ class picasApp:
             swatch_frame.pack(side="left", padx=5)
 
             btn = tk.Button(swatch_frame, bg=hex_code, width=8, height=4,
-                            command=lambda c=color_name, h=hex_code: self.speak_color(c,h))
+                            command=lambda c=color_name, h=hex_code: self.speak_color(c, h))
             btn.pack()
 
             lbl = tk.Label(swatch_frame, text=hex_code, font=("Consolas", 9))
+            lbl.pack()
 
     def speak_color(self, name, hex_code):
-        print(f"User clicked: {name} ({hex_code}") # debugging
+        print(f"User clicked: {name} ({hex_code}")  # debugging
 
         text_to_say = f"{name}"
 
         def run_speech():
             if self.tts_engine:
-                self.tts_engine.say(text_to_say)
-                self.tts_engine.runAndWait()
+                try:
+                    if self.tts_engine._inLoop:
+                        self.tts_engine.endLoop()
 
-        threading.Thread(target=run_speech, daemon=True).start()
+                    self.tts_engine.say(text_to_say)
+                    self.tts_engine.runAndWait()
+                except Exception as e:
+                    print("TTS Error", e)
 
-    def _setup_ui(self):
-        header_frame = tk.Frame(self.root, pady=10)
-        header_frame.pack()
-
-        btn_load = tk.Button(header_frame, text="Uplaod Image", command=self.upload_image,
-                             bg="#4a90e2", fg="white", font=("Arial", 12, "bold"))
-        btn_load.pack()
-
-        self.image_container = tk.Label(self.root, text="No selected image", bg="f0f0f0")
-        self.image_container.pack(pady=20, expand=True)
-
-        self.palette_frame = tk.Frame(self.root)
-        self.pallete_frame.pack(pady=20, fill="x")
-
-        lbl_info = tk.Label(self.palette_frame, text="Dominant colours (click for audio):",
-                            font=("Arial", 10, "italic"))
-        lbl_info.pack(pady=5)
-
-        self.swatches_container = tk.Frame(self.palette_frame)
-        self.swatches_container.pack()
+        if self.tts_engine:
+            threading.Thread(target=run_speech, daemon=True).start()
--- utils.py
+++ utils.py
@@ -7,26 +7,86 @@ def rgb_to_hex(rgb_tuple):
 """Find colour name and closest one from a list. Calculate Euclidian distance between the real and found colour"""
 def get_closest_color_name(requested_rgb):
     colors_db = {
-        (0, 0, 0): "Black",
-        (255, 255, 255): "White",
+        # --- Reds & Pinks ---
+        (139, 0, 0): "Dark Red",
         (255, 0, 0): "Red",
-        (0, 255, 0): "Green",
-        (0, 0, 255): "Blue",
-        (255, 255, 0): "Yellow",
-        (0, 255, 255): "Cyan",
-        (255, 0, 255): "Magenta",
-        (192, 192, 192): "Silver",
-        (128, 128, 128): "Gray",
-        (128, 0, 0): "Maroon",
-        (128, 128, 0): "Olive",
-        (0, 128, 0): "Dark Green",
-        (128, 0, 128): "Purple",
-        (0, 128, 128): "Teal",
-        (0, 0, 128): "Navy",
+        (255, 99, 71): "Tomato",
+        (255, 192, 203): "Pink",
+        (255, 105, 180): "Hot Pink",
+        (199, 21, 133): "Medium Violet Red",
+        (219, 112, 147): "Pale Violet Red",
+
+        # --- Oranges ---
+        (255, 69, 0): "Orange Red",
+        (255, 140, 0): "Dark Orange",
         (255, 165, 0): "Orange",
-        (245, 245, 220): "Beige",
-        (165, 42, 42): "Brown"
+        (255, 215, 0): "Gold",
+
+        # --- Yellows ---
+        (255, 255, 0): "Yellow",
+        (240, 230, 140): "Khaki",
+
+        # --- Purples  ---
+        (230, 230, 250): "Lavender",
+        (216, 191, 216): "Thistle",
+        (221, 160, 221): "Plum",
+        (238, 130, 238): "Violet",
+        (218, 112, 214): "Orchid",
+        (255, 0, 255): "Magenta",
+        (186, 85, 211): "Medium Orchid",
+        (147, 112, 219): "Medium Purple",
+        (138, 43, 226): "Blue Violet",
+        (75, 0, 130): "Indigo",
+        (128, 0, 128): "Purple",
+        (102, 51, 153): "Rebecca Purple",
+        (72, 61, 139): "Dark Slate Blue",
+
+        # --- Greens ---
+        (173, 255, 47): "Green Yellow",
+        (50, 205, 50): "Lime Green",
+        (0, 255, 0): "Lime",
+        (34, 139, 34): "Forest Green",
+        (0, 128, 0): "Green",
+        (0, 100, 0): "Dark Green",
+        (128, 128, 0): "Olive",
+        (107, 142, 35): "Olive Drab",
+        (46, 139, 87): "Sea Green",
+
+        # --- Blues / Cyans ---
+        (0, 255, 255): "Cyan",
+        (0, 206, 209): "Dark Turquoise",
+        (64, 224, 208): "Turquoise",
+        (95, 158, 160): "Cadet Blue",
+        (70, 130, 180): "Steel Blue",
+        (176, 196, 222): "Light Steel Blue",
+        (135, 206, 235): "Sky Blue",
+        (30, 144, 255): "Dodger Blue",
+        (0, 0, 255): "Blue",
+        (0, 0, 205): "Medium Blue",
+        (0, 0, 139): "Dark Blue",
+        (25, 25, 112): "Midnight Blue",
+
+        # --- Browns ---
+        (255, 248, 220): "Cornsilk",
+        (222, 184, 135): "Burlywood",
+        (244, 164, 96): "Sandy Brown",
+        (210, 105, 30): "Chocolate",
+        (160, 82, 45): "Sienna",
+        (165, 42, 42): "Brown",
+        (128, 0, 0): "Maroon",
+
+        # --- Whites / Greys / Blacks ---
+        (255, 255, 255): "White",
+        (245, 245, 245): "White Smoke",
+        (220, 220, 220): "Gainsboro",
+        (192, 192, 192): "Silver",
+        (169, 169, 169): "Dark Gray",
+        (128, 128, 128): "Gray",
+        (105, 105, 105): "Dim Gray",
+        (47, 79, 79): "Dark Slate Gray",
+        (0, 0, 0): "Black"
     }
+
     min_distance = float('inf')
     cloest_name = "Unknown"
 
