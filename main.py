69ffb180d1510dee4c975e60594dcb18680d8b7b
andrei <at3006@yahoo.com>
2026-01-05 17:28:42 +0200

    Quick fix

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 from gui import PicasApp
-# Run the app
+
+"""
+@file main.py
+@brief Run the app.
+"""
+
 if __name__ == '__main__':
     root = tk.Tk() # main window
     app = PicasApp(root) # init app
